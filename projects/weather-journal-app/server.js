8008cd02a1c199f298c8b4c46e0e0df66ef2c83b
Osayd <osaidhamidi@gmail.com>
2025-03-11 08:11:15 +0200

    final

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -1,9 +1,16 @@
 // Setup empty JS object to act as endpoint for all routes
+
 projectData = {};
 
-// Require Express to run server and routes
+// Require Express and CORS
+const express = require('express');
+const cors = require('cors');
 
-// Start up an instance of app
+// Bbody parser
+const bodyParser = require('body-parser');
+
+// Start an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +18,27 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
-// Initialize the main project folder
+// Serve static files from the 'website' directory
 app.use(express.static('website'));
 
+// Server's port
+const port = 8000;
 
-// Setup Server
+// Start the server
+const server = app.listen(port, function () {  
+  console.log(`Server is running on port: ${port}`);
+});
+
+// GET 
+app.get('/all', function (request, response) {
+  response.send(projectData);
+});
+
+// POST 
+app.post('/add', function (request, response) {
+  projectData = request.body;
+  console.log(projectData);
+  response.send(projectData);
+});
